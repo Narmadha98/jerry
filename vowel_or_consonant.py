63d375073cde48529c10b06234c056a63599a811
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-07 14:53:32 +0530

    Create vowel_or_consonant.py

---
+++ vowel_or_consonant.py
@@ -0,0 +1,9 @@
+s=input()
+l=['a','e','i','o','u']
+d=['b','c','d','f','g','h','i','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if(s in l):
+  print("Vowel")
+elif s in d:
+  print("Consonant")
+else:
+  print("Invalid")
