e49333e7a0aa01bfc0ff986297139d7d71decd64
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-08 15:43:55 +0530

    Update even_number.py

--- even_number.py
+++ even_number.py
@@ -1,7 +1,13 @@
 n,m=map(int,input().split())
+l=[]
 for i in range(n+1,m):
-	if(i%2==0):
-		print(i,end=' ')
+	if(i!=m-1):
+		if(i%2==0):
+			print(i,end='')
+	else:
+		if(i%2==0):
+			print(i)
+	
 
 		
 	
