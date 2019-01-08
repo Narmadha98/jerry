ace0b6d6d4e938192173e8a85fb06ebf32ef0c67
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-08 16:05:44 +0530

    Update odd_number.py

--- odd_number.py
+++ odd_number.py
@@ -1,8 +1,9 @@
+s=""
 n,m=map(int,input().split())
 for i in range(n+1,m):
-	if(i!=m-1):
-		if(i%2==1):
-			print(i,end='')
-	else:
-		if(i%2==1):
-			print(i)
+	if(i%2==1):
+		s=s+str(i)+" "
+d=s.strip()
+print(d)
+		
+	
