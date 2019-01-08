114e0209d92d98259e07ccf9147d0405ec50d729
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-08 18:40:36 +0530

    Update mul_5.py

--- mul_5.py
+++ mul_5.py
@@ -2,8 +2,9 @@ n=int(input())
 f=n*5
 if(n==0):
 	print("0 0 0 0 0")
-for i in range(1,6):
-	if(i*n==f):
-		print(i*n)
-	else:
-		print(i*n,end=" ")
+else:
+    for i in range(1,6):
+            if(i*n==f):
+                    print(i*n)
+            else:
+                    print(i*n,end="*")
