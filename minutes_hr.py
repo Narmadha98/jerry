69559f171ae8dee3db619adfa49e3f825d103f9d
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-09 22:07:39 +0530

    Update minutes_hr.py

--- minutes_hr.py
+++ minutes_hr.py
@@ -1,8 +1,7 @@
 n=int(input())
-if(n%60<60):
-    print("0"+" "+str(n))
-if(n%60==0):
-    print(str(n//60))
 if(n>60):
     print(str(n//60)+" "+str(n%60))
-    
+elif(n%60<60):
+    print("0"+" "+str(n))
+elif(n%60==0):
+    print(str(n//60))
