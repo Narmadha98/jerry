54d43d8e8436a45d2376039fd847ccc35d566c75
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-09 19:54:33 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,3 +1,25 @@
-n=int(input())
-l=list(map(int,input().split()))
-print(sorted(l))
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n, a, i;
+    int counts[1000] = {0};
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++){
+        scanf("%d", &a);
+        counts[a]++;
+    }
+    for (i = 0; i < 1000; i++)
+        while (counts[i] > 0){
+            printf("%d ", i);
+            counts[i]--;
+        }
+    printf("\n");
+    
+    return 0;
+}
+
