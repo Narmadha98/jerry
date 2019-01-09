c8cf337a746ed3ea937c4bb531d040f01de0f5d7
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-09 20:09:36 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=sorted(l)
+a=0
+b=len(s)-1
+if(len(s)%2==1):
+    mid=(a+b)//2
+    mid+=1
+    print(l[mid])
+else:
+    c=n-1//2
+    d=n//2
+    print((c+d)//2)
+  
+  
