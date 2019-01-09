0f6c76aa948f4ab32a424d8fbe2e4e7a1f906221
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-09 20:15:38 +0530

    Create sort2.c

---
+++ sort2.c
@@ -0,0 +1,22 @@
+#include<cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n, a, i;
+    int counts[100000] = {0};
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++){
+        scanf("%d", &a);
+        counts[a]++;
+    }
+    for (i = 0; i < 100000; i++)
+        while (counts[i] > 0){
+            printf("%d ", i);
+            counts[i]--;
+        }
+    printf("\n");
+    
+    return 0;
+}
