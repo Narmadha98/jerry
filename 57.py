6ff5544d109017d807e7557deb4231abd1c04ec4
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-14 19:07:11 +0530

    Create 57.py

---
+++ 57.py
@@ -0,0 +1,14 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+c=0
+for i in l:
+    if(i==k):
+        c=c+1
+print(c)
+'''
+
+if( k in l):
+       print("yes")
+else:
+       print("no")
+'''
