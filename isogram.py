5e5049f0064b88a83cf3f6a8af5c87d801b44305
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-01-30 06:04:57 +0530

    Create isogram.py

---
+++ isogram.py
@@ -0,0 +1,14 @@
+def isogram(s):
+    x=s.lower()
+    l=[]
+    for i in x:
+        if i in l:
+            return False
+        l.append(i)
+    return True
+s=input()
+a=isogram(s)
+if(a=="True"):
+    print("yes")
+else:
+    print("no")
