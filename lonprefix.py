cc84f6d5c8bded03de6e6ad9622e4ec1f16ce2dd
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-02 11:29:23 +0530

    Create lonprefix.py

---
+++ lonprefix.py
@@ -0,0 +1,37 @@
+n=int(input())
+l=[]
+z=0
+for i in range(0,n):
+    s=input()
+    l.append(s)
+#print(l)
+q=[]
+for i in l:
+    q.append(len(i))
+#print(q)
+min1=min(q)
+#print(min1)
+p=[]
+for i in range(min1):
+    for j in range(1,n):
+        if(l[0][i]==l[j][i]):
+            p.append(l[0][i])
+            z=1
+        else:
+            z=0
+            break
+    if(z==0):
+        break
+p=set(p)
+m=""
+for i in p:
+    m=m+i
+print(m)
+
+
+            
+            
+        
+   
+
+            
