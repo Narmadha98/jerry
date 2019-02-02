973a0c10c47380b9f290344f767cfb1c6f86fee8
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-02 09:11:38 +0530

    Update h_s1_1.py

--- h_s1_1.py
+++ h_s1_1.py
@@ -1,12 +1,19 @@
 n=int(input())
 l=list(map(int,input().split()))
 r=[]
+f=""
 for i in l:
     if(l.count(i)>1):
         r.append(i)
-s=set(r)
-s=list(s)
-print(sorted(s))
+if(len(r)==0):
+    print("unique")
+else:
+    s=set(r)
+    s=list(s)
+    s=sorted(s)
+    for i in s:
+        f=f+str(i)+" "
+    print(f.strip())
 
     
        
