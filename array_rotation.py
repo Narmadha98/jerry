cc087a2f1e2f075ed94c99ba74df3ccd0f0ea985
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-03 19:19:32 +0530

    Update array_rotation.py

--- array_rotation.py
+++ array_rotation.py
@@ -1,7 +1,12 @@
+def rightarr(l,k,n):
+    for i in range(k):
+        rightrotone(l,n)
+    print(l)
+def rightrotone(l,n):
+    temp=l[n-1]
+    for i in range(n-1 ,0,-1):
+        l[i]=l[i-1]
+    l[0]=temp
 n,k=map(int,input().split())
 l=list(map(int,input().split()))
-t=[]
-t=l[-k:]
-del l[-k:]
-s=t+l
-print(*s)
+s=rightarr(l,k,n)
