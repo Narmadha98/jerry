d96738e73357adbe94637133d6213c41347f51db
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-03 14:24:02 +0530

    Update h_s1_7.py

--- h_s1_7.py
+++ h_s1_7.py
@@ -1,9 +1,14 @@
 n=int(input())
 l=list(map(int,input().split()))
 s=[]
+a=""
 for i in range(0,len(l)):
     if(l[i]%2==0 and i%2==1):
         s.append(l[i])
     elif (l[i]%2==1 and i%2==0):
         s.append(l[i])
-print(*s)
+for i in s:
+    a=a+str(i)+" "
+print(a.strip())
+    
+ 
