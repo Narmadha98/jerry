cdf18a48f3ab12e2fdbd8cacb5a44dc808091a04
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-03 22:15:35 +0530

    Create  prime_factor.py

---
+++ prime_factor.py
@@ -0,0 +1,20 @@
+def isPrime(n):
+    c=1
+    for i in range(2,n):
+        if(n%i==0):
+            c=0
+            break
+    return c
+x=int(input())
+s=""
+l=[]
+for i in range(2,x+1):
+    if(x%i==0):
+        keerthi=isPrime(i)
+        if(keerthi==1):
+            l.append(i)
+for i in sorted(l):
+    s=s+str(i)+" "
+print(s.strip())
+        
+        
