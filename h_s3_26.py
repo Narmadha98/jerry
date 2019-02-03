a41aa999cd32bad3225fb2696645a6937c15f1ba
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-03 08:19:33 +0530

    Update h_s3_26.py

--- h_s3_26.py
+++ h_s3_26.py
@@ -5,11 +5,9 @@ a=""
 b=""
 for i in range(len(s)):
     a=a+str(s[i])+"->"
-#print(a)
 a1=list(a)
-a1.pop(len(a)-1)
+a1.pop(len(a1)-1)
 a1.pop(len(a1)-1)
 for i in a1:
-    b=b+str(i)
+    b=b+i
 print(b)
-
