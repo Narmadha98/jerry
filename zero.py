b447c7d1da5396e77a12bc619a363f01b6d74ade
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-06 20:47:36 +0530

    Update zero.py

--- zero.py
+++ zero.py
@@ -1,10 +1,15 @@
 n=int(input())
 a=list(map(int,input().split()))
-for i in range(n):
-    for j in range(n):
-        if(i!=j):
-            if a[i]+a[j]==0:
-                print(a[i],a[j])
-                break
+m=max(a)
+b=0
+c=0
+for i in range(0,n-1):
+    for j in range(i+1,n):
+            if abs(a[i]+a[j])<m:
+                m=abs(a[i]+a[j])
+                b=a[i]
+                c=a[j]
+print(b,c)
+                
             
                
