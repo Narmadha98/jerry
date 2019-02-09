6be36f6dcdfa650e1b193696960fa990fc32eb55
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-09 20:05:00 +0530

    Create triplets.py

---
+++ triplets.py
@@ -0,0 +1,12 @@
+c=[]
+x=[]
+n=int(input())
+a=list(map(int,input().split()))
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if a[i]< a[j] and a[j]<a[k]:
+                  x=[a[i],a[j],a[k]]
+                  c.append(x)
+print(len(c)-1)
+
