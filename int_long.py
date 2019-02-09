a2e4fb210295ee938711ce61b07e0d1602a6e408
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-09 18:32:28 +0530

    Update int_long.py

--- int_long.py
+++ int_long.py
@@ -1,6 +1,8 @@
 n=int(input())
-if(n>=-2147483648 and n<=2147483648):
+if(n>=-2**15+1  and n<=2**15+1):
     print ("INT")
+elif n>=-2**31+1 and n<=2**31+1:
+    print("LONG")  
 else:
-    print ("LONG")
+    print ("LONG LONG")
 #klsgjmg;
