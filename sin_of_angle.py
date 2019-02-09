2be88a8d9c8faa49cedfd71086dbae1a627b67c8
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-09 13:10:42 +0530

    Update sin_of_angle.py

--- sin_of_angle.py
+++ sin_of_angle.py
@@ -1,7 +1,8 @@
 import math
 n=int(input())
 x=n*(math.pi/180)
-if x <1 and x > 0:
-    print(round(math.sin(x)),1)
+#print(x)
+if x<1 and x > 0:
+    print(round(math.sin(x),1))
 else:
     print(round(math.sin(x)))
