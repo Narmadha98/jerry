8a144381e4c73680fc08a70a9f835ffb9afa84ef
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-12 18:24:18 +0530

    Update lonprefix.py

--- lonprefix.py
+++ lonprefix.py
@@ -4,13 +4,10 @@ z=0
 for i in range(0,n):
     s=input()
     l.append(s)
-#print(l)
 q=[]
 for i in l:
     q.append(len(i))
-#print(q)
 min1=min(q)
-#print(min1)
 p=[]
 for i in range(min1):
     for j in range(1,n):
@@ -24,15 +21,22 @@ for i in range(min1):
         break
 p=set(p)
 p=sorted(p)
+x1=[]
+for i in l:
+    if len(i)==min1:
+        a1=i
+        break
+for i in range(min1):
+    for j in range(len(p)):
+        if a1[i]==p[j]:
+             x1.append(a1[i])
+             
 m=""
-for i in p:
+for i in x1:
     m=m+i
 print(m)
-#myveryowncodexmvlxkvjm 
+
 
             
             
         
-   
-
-            
