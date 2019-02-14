ddda999ffb19cabdd8206824f3d03d31bdaf37b3
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-14 13:56:14 +0530

    Update mini_max.py

--- mini_max.py
+++ mini_max.py
@@ -1,6 +1,11 @@
 # your code goes here
 input()
+l=[]
 a=list(map(int,input().split()))
-s=sum(a)
-so=sorted(a)
-print(s-so[0])
+for i in range(0,len(a)-1):
+	if a[i]>a[i+1]:
+		l.append(a[i])
+	else:
+		l.append(a[i+1])
+print(sum(l))
+	
