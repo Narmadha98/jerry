ba0646de5df2d5fca5c3e8a7c38c63e047e350a0
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-15 21:33:31 +0530

    Update map_abcd.py

--- map_abcd.py
+++ map_abcd.py
@@ -4,4 +4,10 @@ c=0
 for i in range(0,len(s)):
     if int(s[i:i+2])<26 and len(str(int(s[i:i+2])))==2:
         c=c+1
-print(c+c//2+1)
+if c==2:
+    print(c+c//2)
+else:
+    print(c+c//2+1)
+
+
+    
