e206d41e058f14cc49e66971ca072b91b331ac3e
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-16 10:50:47 +0530

    Update bin_sort.py

--- bin_sort.py
+++ bin_sort.py
@@ -1,8 +1,10 @@
 n=int(input())
 l=list(map(int,input().split()))
 c=[[x,bin(x),str(bin(x)).count('1')] for x in l]
-s=sorted(c,key=lambda x:(x[0],x[2]),reverse=True)
+s=sorted(c,key=lambda x:x[2],reverse=True)
+for i in s:
+    del i[1]
 for i in s:
     print(i[0])
-
+               
 #hi guviii:)
