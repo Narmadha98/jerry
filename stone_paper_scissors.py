74863c3923fb1fea968ba90deca827e01282da3b
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-16 14:19:31 +0530

    Create stone_paper_scissors.py

---
+++ stone_paper_scissors.py
@@ -0,0 +1,9 @@
+s1,s2=map(str,input().split())
+if s1=='R' and s2=='P':
+    print('P')
+elif s1=='S' and s2=='P':
+    print('S')
+elif s1=='R' and s2=='S':
+    print('R')
+    
+#rock paper scissior
