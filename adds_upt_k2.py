e3c5abbe4b8d4548b734285341a105a8f92ec158
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-17 00:24:21 +0530

    Create adds_upt_k2.py

---
+++ adds_upt_k2.py
@@ -0,0 +1,12 @@
+import sys
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+for i in range(n):
+    for j in range(n):
+        if i!=j and l[i]+l[j]==k:
+            print('YES')
+            sys.exit(0)
+print('NO')
+            
+
+#hiiiklxfsljklfjkmflkm
