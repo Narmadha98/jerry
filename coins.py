bb23fba688d50579ec61021acaddaee271a43596
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-16 11:55:46 +0530

    Create coins.py

---
+++ coins.py
@@ -0,0 +1,13 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+c=0
+co=0
+l=sorted(l,reverse=True)
+for i in range(n):
+    while(c<k):
+        c=c+l[i]
+        co=co+1
+    if c>k:
+        c=c-l[i]
+        co=co-1
+print(co)
