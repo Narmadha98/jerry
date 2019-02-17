b7c07c8bc71a5f07ce8f90a51c4edfbb442c478b
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-17 21:10:50 +0530

    Update again_palindrome.py

--- again_palindrome.py
+++ again_palindrome.py
@@ -9,9 +9,9 @@ c=[]
 for i in f:
     c.append(len(i))
 #print(f)
-f=sorted(f,key=len)
+#f=sorted(f,key=len)
 #print(f)
-c=sorted(c)
+#c=sorted(c)
 for i in range(len(c)):
     if c[i]!=1:
-        print(f[i])
+        print(f[i],end='\n')
