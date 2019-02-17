b55aa52ff73f542b80d7185840ac6c34d8bbdaa3
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-17 14:20:00 +0530

    Update catalan.py

--- catalan.py
+++ catalan.py
@@ -1,17 +1,18 @@
+
+#catalan_numbers
+#Cn+1=C0Cn+C1Cn+....+CnC0
 def catalan(n):
-  if n<=1:
-    return 1
-  else:
-    r=0
-    for i in range(n):
-      r+=catalan(i)*catalan(n-i-1)
-    return r
+    if n<=1:
+        return 1
+    else:
+        s=0
+        for i in range(n):
+            s=s+catalan(i)*catalan(n-i-1)
+        return s
 n=int(input())
-if n<=1:
-  n+=1
-else:
-  n=n
-li=[]
+if n==0:
+    n=1
+c=[]
 for i in range(n):
-  li.append(catalan(i))
-print(*li)
+    c.append(catalan(i))
+print(*c)
