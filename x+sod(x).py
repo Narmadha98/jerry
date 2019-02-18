fa9f23223c821d7ce025c9c15773b160742a54d8
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-18 19:28:58 +0530

    Create x+sod(x).py

---
+++ x+sod(x).py
@@ -0,0 +1,16 @@
+def digsum(i):
+    s=0
+    for k in range(len(i)):
+        s=s+int(i[k])
+    return s
+n=int(input())
+f=[]
+for i in range(n):
+    o=str(i)
+    if(n==i+digsum(o)):
+        f.append(i)
+if len(f)>0:
+    for i in f:
+        print(i)
+else:
+    print(-1)
