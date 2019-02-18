27dac3dde522f36a24ce9b0ce53336c18cb7415a
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-18 20:35:05 +0530

    Create pro_30.py

---
+++ pro_30.py
@@ -0,0 +1,24 @@
+import sys
+from itertools import combinations
+def comb(n,k):
+    sp=""
+    n=list(n)
+    p=list(combinations(n,k))
+    return p
+n=input()
+if int(n)%8==0:
+    print('yes')
+else:
+    for k in range(len(n)-1,0,-1):
+        s=comb(n,k)
+        for i in s:
+            o=list(i)
+            sp=""
+            for f in o:
+                sp=sp+f
+            if(int(sp)%8==0):
+                print('yes')
+                sys.exit()
+print('no')                
+            
+    
