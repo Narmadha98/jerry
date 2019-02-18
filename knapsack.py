8bd397dd0bb05076df781c5f5a7f639fa67489de
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-18 23:06:33 +0530

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,12 +1,18 @@
 n,w=map(int,input().split())
-we=list(map(int,input().split()))
-va=list(map(int,input().split()))
-s=0
-op=0
+p=list(map(int,input().split()))
+v=list(map(int,input().split()))
+r=[]
+c=0
 for i in range(n):
-    s+=we[i]
-    if w<s:
-        break
-    op+=va[i]
-print(op)
+    x=v[i]/p[i]
+    r.append(x)
+while w>=0 and len(r)>0:
+    mindex=r.index(max(r))
+    if w>=p[mindex]:
+        c=c+v[mindex]
+        w=w-p[mindex]
+    p.pop(mindex)
+    v.pop(mindex)
+    r.pop(mindex)
+print(c)
     
