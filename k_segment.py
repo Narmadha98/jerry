f0994685756eeaa5d61e01a22cfc415620c253e9
Narmadha98 <36038617+Narmadha98@users.noreply.github.com>
2019-02-22 17:11:14 +0530

    Update k_segment.py

--- k_segment.py
+++ k_segment.py
@@ -1,6 +1,9 @@
 n,k=map(int,input().split())
 l=list(map(int,input().split()))
-s=[]
-for i in range(0,n,2):
-    s.append(min((l[i:k+i])))
-print(max(s))
+if k==1:
+    print(min(l))
+elif k==2:
+    print(max(l[0],l[n-1]))
+else:
+    print(max(l))
+    
